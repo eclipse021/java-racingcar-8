2b79406a4a5ac168ffc264fdf46bc072857194b3
eclipse021 <yahang2008@naver.com>
2025-10-27 10:03:22 +0900

    feat: 자동차 이름의 유효성을 검사하는 로직 추가

--- src/main/java/racingcar/domain/model/Car.java
+++ src/main/java/racingcar/domain/model/Car.java
@@ -2,13 +2,33 @@ package racingcar.domain.model;
 
 public class Car {
 
+    private static final int MIN_REQUIREMENTS = 4;
+    private static final int MAX_NAME_LENGTH = 5;
+
     private final String name;
     private final CarStatus carStatus;
 
     public Car(String name) {
+        validateName(name);
         this.name = name.trim();
         this.carStatus = new CarStatus();
     }
 
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 비어 있을 수 없습니다.");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public CarStatus getCarStatus() {
+        return this.carStatus;
+    }
 
 }
