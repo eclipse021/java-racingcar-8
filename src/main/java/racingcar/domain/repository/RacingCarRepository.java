1bd3f673629a76e31772629ef496527698a96471
eclipse021 <yahang2008@naver.com>
2025-10-27 10:21:34 +0900

    feat: RacingCarRepository에 addCar 및 findAll 메서드 구현

--- src/main/java/racingcar/domain/repository/RacingCarRepository.java
+++ src/main/java/racingcar/domain/repository/RacingCarRepository.java
@@ -2,13 +2,36 @@ package racingcar.domain.repository;
 
 import racingcar.domain.model.Car;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RacingCarRepository {
 
-    // 자동차 전체 목록 관리하는 collection
+    // 자동차 전체 관리 목록
     private final Map<String, Car> cars = new LinkedHashMap<>();
+
+    /**
+     * 자동차를 저장소에 추가
+     */
+    public void addCar(String name) {
+
+        if(cars.containsKey(name)) {
+            throw new IllegalArgumentException("중복된 자동차 이름입니다: " + name);
+        }
+
+        // 자동차 생성 및 전체 관리 목록에 추가
+        Car car = new Car(name);
+        cars.put(car.getName(), car);
+    }
+
+    /**
+     * 모든 자동차 목록 반환
+     */
+    public List<Car> findAll() {
+        return new ArrayList<>(cars.values());
+    }
 
 
 
