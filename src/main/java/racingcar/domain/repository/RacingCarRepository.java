d188a92410b4bb941dc5d98a60b9e02160c44bb2
eclipse021 <yahang2008@naver.com>
2025-10-27 10:11:33 +0900

    chore: RacingCarRepository 생성

---
+++ src/main/java/racingcar/domain/repository/RacingCarRepository.java
@@ -0,0 +1,18 @@
+package racingcar.domain.repository;
+
+import racingcar.domain.model.Car;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RacingCarRepository {
+
+    // 자동차 전체 목록 관리하는 collection
+    private final Map<String, Car> cars = new LinkedHashMap<>();
+
+
+
+
+
+
+}
