3ccd6328c5e000d4c153cc750935f13f6509c799
eclipse021 <yahang2008@naver.com>
2025-10-27 11:57:16 +0900

    feat: EventPublisher 구현

--- src/main/java/racingcar/domain/event/publisher/EventPublisher.java
+++ src/main/java/racingcar/domain/event/publisher/EventPublisher.java
@@ -1,4 +1,25 @@
 package racingcar.domain.event.publisher;
 
+import racingcar.domain.event.subscriber.EventSubscriber;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventPublisher {
+
+    // Publisher가 관리하는 모든 구독자 목록
+    private static final List<EventSubscriber> subscribers = new ArrayList<>();
+
+    /** 구독자 등록 */
+    public static void subscribe(EventSubscriber subscriber) {
+        subscribers.add(subscriber);
+    }
+
+    /** 이벤트 발행 */
+    public static void publish(Object event) {
+        for (EventSubscriber subscriber : subscribers) {
+            subscriber.onEvent(event); // 각 구독자의 onEvent 호출
+        }
+    }
+
 }
